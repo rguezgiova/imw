b8b68813ce54d1303903081cfbef629a7de70df2
rguezgiova <giovanni_1998_10@hotmail.com>
2019-12-10 13:13:36 +0000

    Empezando a dar forma al index - Iria y Giovanni

--- ut2/a8/main.py
+++ ut2/a8/main.py
@@ -5,41 +5,24 @@ from flask import request
 from vm import VirtualMachine
 
 app = Flask(__name__)
-vmachine = VirtualMachine("Azkaban", 16, 3.7, 1000, "Debian")
+vmachine = VirtualMachine('Azkaban', 16, 3.7, 1000, 'debian')
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", vmachine=vmachine)
+    return render_template('index.html', vmachine=vmachine)
 
 
-@app.route("/change_status/<new_status>")
+@app.route('/change_status/<new_status>')
 def change_status(new_status):
-    if new_status == "0":
-        vmachine.stop()
-    elif new_status == "1":
-        vmachine.start()
-    elif new_status == "2":
-        vmachine.suspend()
-    else:
-        print("Use a correct status")
-
-    return redirect("/")
+    # tu código aquí
+    return redirect('/')
 
 
-@app.route("/run_process", methods=["GET", "POST"])
+@app.route('/run_process', methods=['GET', 'POST'])
 def run_process():
-    if request.method == "POST":
-        pid = int(request.form["pid"])
-        ram = float(request.form["ram"])
-        cpu = float(request.form["cpu"])
-        hdd = float(request.form["hdd"])
-
-        vmachine.run(pid, ram, cpu, hdd)
-        return redirect("/")
+    if request.method == 'POST':
+        # tu código aquí
+        return redirect('/')
     else:
-        return render_template("run_process.html")
-
-
-if __name__ == "__main__":
-app.run(debug=True)
+        return render_template('run_process.html')
