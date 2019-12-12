aaa26e78e7e86b70cebaac57b519c96151fe627f
rguezgiova <giovanni_1998_10@hotmail.com>
2019-12-12 09:16:54 +0000

    Trabajo final primer trimestre acabado. Iria y Giovanni

--- ut2/a8/main.py
+++ ut2/a8/main.py
@@ -15,14 +15,25 @@ def index():
 
 @app.route('/change_status/<new_status>')
 def change_status(new_status):
-    # tu código aquí
+    if new_status == "0":
+        vmachine.stop()
+    elif new_status == "1":
+        vmachine.start()
+    elif new_status == "2":
+        vmachine.suspend()
+    else:
+        print("Use a correct status")
     return redirect('/')
 
 
 @app.route('/run_process', methods=['GET', 'POST'])
 def run_process():
     if request.method == 'POST':
-        # tu código aquí
+        pid = int(request.form["pid"])
+        ram = float(request.form["ram"])
+        cpu = float(request.form["cpu"])
+        hdd = float(request.form["hdd"])
+        vmachine.run(pid, ram, cpu, hdd)
         return redirect('/')
     else:
         return render_template('run_process.html')
